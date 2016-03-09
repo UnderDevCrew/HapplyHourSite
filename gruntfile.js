565fb6a64b278ed57b6e72de9049fec623ec22ba
benjamin botella <b.neb.19@gmail.com>
2016-03-09 23:45:50 +0100

    css tasks configuration in gruntfile.js + init sass && modif package.json

--- gruntfile.js
+++ gruntfile.js
@@ -3,30 +3,53 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     sass: {
-      options: {
-        sourceMap: true
-        },
       dist: {
-        files: {
-          'assets/css/app.css' : 'assets/sass/app.scss'
-        }
+        files: [{
+          expand: true,
+          cwd: 'www/sass',
+          src: ['*.scss', '!_*.scss'],
+          dest: 'www/css',
+          ext: '.css'
+        }]
       }
     },
+
+    concat: {
+      //options: {
+      //  separator: ';',
+      //},
+      dist: {
+        src: ['www/css/*.css'],
+        dest: 'www/css/style.css',
+      },
+    },
+
+    cssmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'www/css',
+          src: ['style.css', '!*.min.css'],
+          dest: 'www/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+
     watch: {
       css: {
-        files: '**/*.scss',
-        tasks: ['sass']
+        files: 'www/sass/*.scss',
+        tasks: ['css-tasks']
       }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Load plugins
+  require('load-grunt-tasks')(grunt);
 
   // Default task(s).
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('css-tasks', ['sass:dist', 'concat:dist', 'cssmin:dist']);
 
 };
